bed67e571a5c91216e22cd4a0b0a8d7a1e32a388
Lucas Moura Pereira <lucasmoura@ioasys.com.br>
2022-08-22 22:18:17 -0300

    gravar em csv

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from modules.fetch import fetch_data
 from modules.utils import build_json
+from modules.csv import build_csv
 
 if __name__ == '__main__':
   raw_query_data = fetch_data(
     page_size = 10,
-    max_value = 100
+    max_value = 1000
   )
   build_json(raw_query_data)
+  build_csv()
   print('Done')
