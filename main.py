8cb53a6889e5d45b0290214f821cf7aabd7bbd37
Thalles Sales <thalles6alves@gmail.com>
2022-09-19 20:22:04 -0300

    casts pagesize params to int

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ from modules.utils import build_json, build_csv
 parser = argparse.ArgumentParser()
 parser.add_argument('--output', dest='output', help='Type of output')
 parser.add_argument('--queryname', dest='queryname', help='Name of the query file', default=None)
+parser.add_argument('--pagesize', dest='pagesize', help='Size of pages', default=10)
+parser.add_argument('--querysize', dest='querysize', help='Size of the query being made', default=10)
 
 args = parser.parse_args()
 
 if __name__ == '__main__':
   raw_query_data = fetch_data(
     query_name = args.queryname,
-    page_size = 10,
-    max_value = 10
+    page_size = int(args.pagesize),
+    max_value = int(args.querysize)
   )
   if args.output == 'json':
     build_json(raw_query_data)
