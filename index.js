ad5f1801725e4580b4b049d9805618d34e75bbe2
Lucas Moura Pereira <lucasmoura@ioasys.com.br>
2022-09-21 12:42:57 -0300

    ajustes

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const { parse } = require("csv-parse");
 const shell = require("shelljs");
 const converter = require("json-2-csv");
 
-const today  = new Date();
+const today = new Date();
 const fields = [];
-const urls = [];
 const csvToJsonResult = [];
-const data = [];
 
 const path = "~/Documentos/pessoal/lab6/lab06-puc-22/repos";
 
@@ -43,33 +41,44 @@ const csvToJson = (csv) => {
 };
 
 const calcCBO = () => {
-  const resp = csvToJsonResult.reduce(
-    (acc, values) => acc + parseInt(values.cbo),
-    0
-  );
+  const resp = csvToJsonResult
+    .filter((result) => !Number.isNaN(parseInt(result.cbo)) === false)
+    .reduce((acc, values) => acc + parseInt(values.cbo), 0);
   return resp;
 };
 
 const calcDit = () => {
-  const resp = csvToJsonResult.reduce(
-    (prev, current) =>
-      !isNaN(current.dit) && parseInt(prev.dit) > parseInt(current.dit)
-        ? prev
-        : current,
-    0
-  ).dit;
+  const resp = csvToJsonResult
+    .filter((result) => Number.isNaN(parseInt(result.dit)) === false)
+    .reduce(
+      (prev, current) =>
+        current.dit && parseInt(prev.dit) > parseInt(current.dit)
+          ? prev
+          : current,
+      0
+    ).dit;
   return parseInt(resp);
 };
 
 const calcLcom_as = () => {
-  const elm = csvToJsonResult.length + 1;
-  const resp = csvToJsonResult.reduce(
-    (acc, values) =>
-      !isNaN(values["lcom*"]) && acc + parseFloat(values["lcom*"]),
-    0
-  );
-  const media = resp / elm;
-  return media;
+  const filted = csvToJsonResult
+    .filter((number) => Number.isNaN(parseFloat(number["lcom*"])) === false)
+    .sort((a, b) => a["lcom*"] - b["lcom*"]);
+  const length = filted.length;
+  const half = Math.floor(length / 2);
+
+  const resp1 = filted[length / 2];
+  const resp2 = filted[length / 2 - 1];
+  const resp3 = parseFloat(resp1["lcom*"]) === undefined ? 0.0 : parseFloat(resp1["lcom*"]) + parseFloat(resp2["lcom*"]);
+  const resp4 = parseFloat(resp2["lcom*"]) === undefined ? 0.0 : parseFloat(resp1["lcom*"]) + parseFloat(resp2["lcom*"]);
+  const resp5 = resp3 + resp4
+  const resp6 = resp5 / 2.0;
+
+  if (length % 2) {
+    return  parseFloat(resp1[half]["lcom*"]);
+  } else {
+    return resp6;
+  }
 };
 
 const calcLoc = () => {
@@ -83,15 +92,18 @@ const calcLoc = () => {
 fs.createReadStream("./out/lab2.csv")
   .pipe(parse({ delimiter: ",", from_line: 2 }))
   .on("data", (row) => {
-    row.map((field) => {
-      fields.push(field);
+    fields.push({
+      name: row[0],
+      operatingTime: row[1],
+      popularity: row[2],
+      url: row[3],
+      releases: row[4],
     });
-    row.map((url) => url.includes("https") && urls.push(url));
   })
   .on("end", () => {
-    urls.map((url, i) => {
+    fields.map((field) => {
       shell.cd("./repos");
-      shell.exec(`git clone ${url}`);
+      shell.exec(`git clone ${field.url}`);
       shell.cd("../ck/target");
       shell.exec(
         `java -jar ck-0.7.1-SNAPSHOT-jar-with-dependencies.jar ${path} true 0 False`
@@ -105,30 +117,30 @@ fs.createReadStream("./out/lab2.csv")
       const csvMethod = fs.readFileSync("./ck/target/method.csv");
       csvToJson(csvMethod);
       const loc = calcLoc();
-      const createData  = new Date(fields[1]);
-      data.push({
-        name: fields[0],
+      const createData = new Date(field.operatingTime);
+      const data = {
+        name: field.name,
         operatingTime: today.getFullYear() - createData.getFullYear(),
-        popularity: fields[2],
-        releases: fields[4], 
-        url: url,
+        popularity: field.popularity,
+        releases: field.releases,
+        url: field.url,
         cbo: cbo,
         dit: dit,
         lcom_as: lcom_as,
         loc: loc,
+      };
+
+      converter.json2csv(data, (err, csv) => {
+        if (err) {
+          throw err;
+        }
+
+        // write CSV to a file
+        fs.writeFileSync("./out/Arquivo.csv", csv, { flag: "a+" });
       });
+
       shell.rm("-rf", "./repos/*");
     });
-
-    converter.json2csv(data, (err, csv) => {
-      if (err) {
-        throw err;
-      }
-
-      // write CSV to a file
-      fs.writeFileSync("./out/Arquivo.csv", csv);
-    });
-
   })
   .on("error", (error) => {
     console.log(error.message);
