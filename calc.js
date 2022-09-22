68bbe0b64a976f916b70f277e30cf8ae03f6150d
Lucas Moura Pereira <lucasmoura@ioasys.com.br>
2022-09-21 23:53:23 -0300

    ajuste no calculo

---
+++ calc.js
@@ -0,0 +1,97 @@
+const csvToJsonResult = [];
+
+const csvToJson = (csv) => {
+  const array = csv.toString().split("\n");
+  const headers = array[0].split(",");
+
+  for (let i = 1; i < array.length - 1; i++) {
+    const jsonObject = {};
+    const currentArrayString = array[i];
+
+    let string = "";
+
+    let quoteFlag = 0;
+    for (let character of currentArrayString) {
+      if (character === '"' && quoteFlag === 0) {
+        quoteFlag = 1;
+      } else if (character === '"' && quoteFlag == 1) quoteFlag = 0;
+      if (character === "," && quoteFlag === 0) character = "|";
+      if (character !== '"') string += character;
+    }
+    let jsonProperties = string.split("|");
+
+    for (let j in headers) {
+      if (jsonProperties[j].includes(", ")) {
+        jsonObject[headers[j]] = jsonProperties[j]
+          .split(", ")
+          .map((item) => item.trim());
+      } else jsonObject[headers[j]] = jsonProperties[j];
+    }
+    csvToJsonResult.push(jsonObject);
+  }
+};
+
+// mediana
+const calcCBO = () => {
+  const resp = csvToJsonResult
+    .filter((result) => Number.isNaN(parseInt(result.cbo)) !== true)
+    .sort((a, b) => a.cbo - b.cbo);
+
+  const half = Math.floor(resp.length / 2);
+  let item = null;
+
+  if (resp.length % 2) {
+    item = resp[half];
+    return parseInt(item.cbo);
+  } else {
+    item = (resp[half - 1] + resp[half]) / 2.0;
+    return parseInt(item.cbo);
+  }
+};
+
+// moda
+const calcDit = () => {
+  const resp = csvToJsonResult
+    .filter((result) => Number.isNaN(parseInt(result.dit)) !== true)
+    .reduce(
+      (prev, current) =>
+        current.dit && parseInt(prev.dit) > parseInt(current.dit)
+          ? prev
+          : current,
+      0
+    ).dit;
+  return parseInt(resp);
+};
+
+//madiana
+const calcLcom_as = () => {
+  const resp = csvToJsonResult
+    .filter((result) => Number.isNaN(parseFloat(result["lcom*"])) !== true)
+    .sort((a, b) => a["lcom*"] - b["lcom*"]);
+
+  const half = Math.floor(resp.length / 2);
+  let item = null;
+
+  if (resp.length % 2 === 1) {
+    item = resp[half];
+    return parseFloat(item["lcom*"]);
+  }
+
+  if (resp.length % 2 === 0) {
+    item =
+      (parseFloat(resp[half - 1]["lcom*"]) + parseFloat(resp[half]["lcom*"])) /
+      2.0;
+    return item;
+  }
+};
+
+// soma
+const calcLoc = () => {
+  const resp = csvToJsonResult.reduce(
+    (acc, values) => acc + parseInt(values.loc),
+    0
+  );
+  return resp;
+};
+
+module.exports = { csvToJson, calcCBO, calcDit, calcLcom_as, calcLoc };
--- index.js
+++ index.js
@@ -1,139 +1,67 @@
+const { csvToJson, calcCBO, calcDit, calcLcom_as, calcLoc } = require("./calc");
 const fs = require("fs");
+const { stringify } = require("csv-stringify");
 const { parse } = require("csv-parse");
 const shell = require("shelljs");
-const converter = require("json-2-csv");
 
 const today = new Date();
-const fields = [];
-const csvToJsonResult = [];
+let data = null;
+
+const columns = [
+  "name",
+  "operatingTime",
+  "popularity",
+  "releases",
+  "cbo",
+  "dit",
+  "lcom_as",
+  "loc",
+];
 
 const path = "~/Documentos/pessoal/lab6/lab06-puc-22/repos";
 
-const csvToJson = (csv) => {
-  const array = csv.toString().split("\n");
-  const headers = array[0].split(",");
-
-  for (let i = 1; i < array.length - 1; i++) {
-    const jsonObject = {};
-    const currentArrayString = array[i];
-
-    let string = "";
-
-    let quoteFlag = 0;
-    for (let character of currentArrayString) {
-      if (character === '"' && quoteFlag === 0) {
-        quoteFlag = 1;
-      } else if (character === '"' && quoteFlag == 1) quoteFlag = 0;
-      if (character === "," && quoteFlag === 0) character = "|";
-      if (character !== '"') string += character;
-    }
-    let jsonProperties = string.split("|");
-
-    for (let j in headers) {
-      if (jsonProperties[j].includes(", ")) {
-        jsonObject[headers[j]] = jsonProperties[j]
-          .split(", ")
-          .map((item) => item.trim());
-      } else jsonObject[headers[j]] = jsonProperties[j];
-    }
-    csvToJsonResult.push(jsonObject);
-  }
-};
-
-const calcCBO = () => {
-  const resp = csvToJsonResult
-    .filter((result) => !Number.isNaN(parseInt(result.cbo)))
-    .reduce((acc, values) => acc + parseInt(values.cbo), 0);
-  return resp;
-};
-
-const calcDit = () => {
-  const resp = csvToJsonResult
-    .filter((result) => !Number.isNaN(parseInt(result.dit)))
-    .reduce(
-      (prev, current) =>
-        current.dit && parseInt(prev.dit) > parseInt(current.dit)
-          ? prev
-          : current,
-      0
-    ).dit;
-  return parseInt(resp);
-};
-
-const calcLcom_as = () => {
-  const concat = csvToJsonResult.sort((a, b) => a["lcom*"] - b["lcom*"]);
-
-  if (concat.length % 2 == 1) {
-    return parseFloat(concat[concat.length / 2 - 0.5]["lcom*"]);
-  } else {
-    return parseFloat (
-      (concat[concat.length / 2]["lcom*"] +
-        concat[concat.length / 2 - 1]["lcom*"]) /
-      2
-    );
-  }
-};
-
-const calcLoc = () => {
-  const resp = csvToJsonResult.reduce(
-    (acc, values) => acc + parseInt(values.loc),
-    0
-  );
-  return resp;
-};
+const writableStream = fs.createWriteStream("./out/Arquivo.csv");
+const stringifier = stringify({ header: true, columns: columns });
 
 fs.createReadStream("./out/lab2.csv")
   .pipe(parse({ delimiter: ",", from_line: 2 }))
   .on("data", (row) => {
-    fields.push({
+    shell.cd("./repos");
+    shell.exec(`git clone ${row[3]}`);
+    shell.cd("../ck/target");
+    shell.exec(
+      `java -jar ck-0.7.1-SNAPSHOT-jar-with-dependencies.jar ${path} true 0 False`
+    );
+    shell.cd("../../");
+    const csvClass = fs.readFileSync("./ck/target/class.csv");
+    csvToJson(csvClass);
+    const cbo = calcCBO();
+    const dit = calcDit();
+    const lcom_as = calcLcom_as();
+    const csvMethod = fs.readFileSync("./ck/target/method.csv");
+    csvToJson(csvMethod);
+    const loc = calcLoc();
+
+    const createData = new Date(row[1]);
+
+    data = {
       name: row[0],
-      operatingTime: row[1],
+      operatingTime: today.getFullYear() - createData.getFullYear(),
       popularity: row[2],
-      url: row[3],
       releases: row[4],
-    });
+      url: row[3],
+      cbo: cbo,
+      dit: dit,
+      lcom_as: lcom_as,
+      loc: loc,
+    };
+
+    stringifier.write(data);
+    stringifier.pipe(writableStream);
   })
   .on("end", () => {
-    fields.map((field) => {
-      shell.cd("./repos");
-      shell.exec(`git clone ${field.url}`);
-      shell.cd("../ck/target");
-      shell.exec(
-        `java -jar ck-0.7.1-SNAPSHOT-jar-with-dependencies.jar ${path} true 0 False`
-      );
-      shell.cd("../../");
-      const csvClass = fs.readFileSync("./ck/target/class.csv");
-      csvToJson(csvClass);
-      const cbo = calcCBO();
-      const dit = calcDit();
-      const lcom_as = calcLcom_as();
-      const csvMethod = fs.readFileSync("./ck/target/method.csv");
-      csvToJson(csvMethod);
-      const loc = calcLoc();
-      const createData = new Date(field.operatingTime);
-      const data = {
-        name: field.name,
-        operatingTime: today.getFullYear() - createData.getFullYear(),
-        popularity: field.popularity,
-        releases: field.releases,
-        url: field.url,
-        cbo: cbo,
-        dit: dit,
-        lcom_as: lcom_as,
-        loc: loc,
-      };
-
-      converter.json2csv(data, (err, csv) => {
-        if (err) {
-          throw err;
-        }
-
-        // write CSV to a file
-        fs.writeFileSync("./out/Arquivo.csv", csv, { flag: "a+" });
-      });
-
-      shell.rm("-rf", "./repos/*");
-    });
+    console.log("Finished writing data");
+    shell.rm("-rf", "./repos/*");
   })
   .on("error", (error) => {
     console.log(error.message);
